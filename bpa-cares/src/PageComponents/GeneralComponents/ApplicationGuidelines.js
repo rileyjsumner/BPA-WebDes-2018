63b14bc2dc64f1faf1837ffb3332c0df4c58ac4e
langnerer <Lagnerer22@gmail.com>
2017-12-26 16:34:01 -0600

    Gen

---
+++ bpa-cares/src/PageComponents/GeneralComponents/ApplicationGuidelines.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Application Guidelines extends Component {
+  render() {
+    return (
+      <div className="awardWrapper">
+        <h1>Application guidelines</h1>
+        <p> All nominations must be submitted on the official nomination form. Nominations must be
+submitted by a current active student member. Nominations due March 1, 2018. Incomplete
+applications will not be considered. Nominations must be submitted at <a href ="http://www.bpa.org/submit." > </a>
+</div>
+)
+}
+}
---
+++ bpa-cares/src/PageComponents/GeneralComponents/Recognition.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Recognition extends Component {
+  render() {
+    return (
+      <div className="awardWrapper">
+        <h1>Recognition</h1>
+        <p> A maximum of one (1) individual will be recognized with a plaque at the National Leadership
+Conference for that year. </p>
+</div>
+)
+}
+}
