87db8bbd4bf6c9b4daf5375a5eb43d7c05b6d25f
Riley Sumner <tornadoriley@gmail.com>
2017-12-26 22:48:27 -0600

    centered

--- bpa-cares/src/PageComponents/NavBar/NavBar.js
+++ bpa-cares/src/PageComponents/NavBar/NavBar.js
@@ -29,40 +29,48 @@ export class Navigation extends Component {
             <Navbar color="faded" light expand="md">
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="col-md-12" vertical>
-                        <UncontrolledDropdown right>
-                            <DropdownToggle className="navLink" color="danger">Service Learning Awards</DropdownToggle>
-                            <DropdownMenu>
-                                <DropdownItem href="/#/ServiceLearning/Community-Service">Community Service Award</DropdownItem>
-                                <DropdownItem href="/#/ServiceLearning/Environmental-Action">Environmental Action/Awareness Award</DropdownItem>
-                                <DropdownItem href="/#/ServiceLearning/Safety-Awareness">Safety Awareness Award</DropdownItem>
-                                <DropdownItem href="/#/ServiceLearning/Service-Learning-Individual">Service Learning Individual Award</DropdownItem>
-                                <DropdownItem href="/#/ServiceLearning/Special-Olympics">Special Olympics Award</DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                        <UncontrolledDropdown right>
-                            <DropdownToggle className="navLink" color="danger">Special Recognition Awards</DropdownToggle>
-                            <DropdownMenu >
-                                <DropdownItem href="/#/SpecialRecognition/BPA-Marketing-and-Public-Relations">BPA Marketing & Public Relations Award</DropdownItem>
-                                <DropdownItem href="/#/SpecialRecognition/BPA-Merit-Scholar">BPA Merit Scholar Award</DropdownItem>
-                                <DropdownItem href="/#/SpecialRecognition/Chapter-Activities-Award-of-Excellence">Chapter Activities Award of Excellence</DropdownItem>
-                                <DropdownItem href="/#/SpecialRecognition/Recruiter-of-the-Year">Recruiter of the Year Award</DropdownItem>
-                                <DropdownItem href="/#/SpecialRecognition/Membership-Explosion">Membership Explosion Award</DropdownItem>
-                                <DropdownItem href="/#/SpecialRecognition/Social-Media">Social Media Award</DropdownItem>
-                                <DropdownItem href="/#/SpecialRecognition/Professional-Cup">Professional Cup</DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                        <UncontrolledDropdown right>
-                            <DropdownToggle className="navLink" color="danger">Professional Awards</DropdownToggle>
-                            <DropdownMenu >
-                                <DropdownItem href="/#/Professional/Advisor-of-the-Year">Advisor of the Year Award</DropdownItem>
-                                <DropdownItem href="/#/Professional/Emerging-Advisor">Emerging Advisor of the Year Award</DropdownItem>
-                                <DropdownItem href="/#/Professional/Hall-Of-Fame">Hall of Fame Award</DropdownItem>
-                                <DropdownItem href="/#/Professional/Oustanding-Service-Award">Outstanding Service Award</DropdownItem>
-                                <DropdownItem href="/#/Professional/Student-of-the-Year">Student of the Year Award</DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
+                    <div className="navAlign">
+                        <Nav className="row" vertical>
+                            <div>
+                                <UncontrolledDropdown className="text-center" right>
+                                    <DropdownToggle className="col-md-12" color="danger">Service Learning Awards</DropdownToggle>
+                                    <DropdownMenu>
+                                        <DropdownItem href="/#/ServiceLearning/Community-Service">Community Service Award</DropdownItem>
+                                        <DropdownItem href="/#/ServiceLearning/Environmental-Action">Environmental Action/Awareness Award</DropdownItem>
+                                        <DropdownItem href="/#/ServiceLearning/Safety-Awareness">Safety Awareness Award</DropdownItem>
+                                        <DropdownItem href="/#/ServiceLearning/Service-Learning-Individual">Service Learning Individual Award</DropdownItem>
+                                        <DropdownItem href="/#/ServiceLearning/Special-Olympics">Special Olympics Award</DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </div>
+                            <div>
+                                <UncontrolledDropdown className="text-center" right>
+                                    <DropdownToggle className="col-md-12" color="danger">Special Recognition Awards</DropdownToggle>
+                                    <DropdownMenu >
+                                        <DropdownItem href="/#/SpecialRecognition/BPA-Marketing-and-Public-Relations">BPA Marketing & Public Relations Award</DropdownItem>
+                                        <DropdownItem href="/#/SpecialRecognition/BPA-Merit-Scholar">BPA Merit Scholar Award</DropdownItem>
+                                        <DropdownItem href="/#/SpecialRecognition/Chapter-Activities-Award-of-Excellence">Chapter Activities Award of Excellence</DropdownItem>
+                                        <DropdownItem href="/#/SpecialRecognition/Recruiter-of-the-Year">Recruiter of the Year Award</DropdownItem>
+                                        <DropdownItem href="/#/SpecialRecognition/Membership-Explosion">Membership Explosion Award</DropdownItem>
+                                        <DropdownItem href="/#/SpecialRecognition/Social-Media">Social Media Award</DropdownItem>
+                                        <DropdownItem href="/#/SpecialRecognition/Professional-Cup">Professional Cup</DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </div>
+                            <div>
+                                <UncontrolledDropdown className="text-center" right>
+                                    <DropdownToggle className="col-md-12" color="danger">Professional Awards</DropdownToggle>
+                                    <DropdownMenu >
+                                        <DropdownItem href="/#/Professional/Advisor-of-the-Year">Advisor of the Year Award</DropdownItem>
+                                        <DropdownItem href="/#/Professional/Emerging-Advisor">Emerging Advisor of the Year Award</DropdownItem>
+                                        <DropdownItem href="/#/Professional/Hall-Of-Fame">Hall of Fame Award</DropdownItem>
+                                        <DropdownItem href="/#/Professional/Oustanding-Service-Award">Outstanding Service Award</DropdownItem>
+                                        <DropdownItem href="/#/Professional/Student-of-the-Year">Student of the Year Award</DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </div>
+                        </Nav>
+                    </div>
                 </Collapse>
             </Navbar>
         );
