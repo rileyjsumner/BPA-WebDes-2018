44c7dc5fb9e350c290fe58a6e9ed02d9efd2b5de
langnerer <Lagnerer22@gmail.com>
2017-12-26 22:55:19 -0600

    fix 95

--- bpa-cares/src/Awards/Special-Recognition/ProfessionalCup.js
+++ bpa-cares/src/Awards/Special-Recognition/ProfessionalCup.js
@@ -7,44 +7,46 @@ export class ProfessionalCup extends Component {
   render() {
     return (
       <div className="awardWrapper">
-      <h1> The Professional Cup Award </h1>
-      <h1> (Outstanding State Participation) </h1>
-      <h2>Purpose</h2>
-      <p>To encourage state associations to become actively involved in promoting participation in the BPA
-Cares Award Program by the local chapters and to recognize states with active participation; and to
-recognize outstanding state participation in BPA programs
-       </p>
-<h2> Eligibility</h2>
-<p>
-Any state association in the Middle Level, Post-Secondary or Secondary Division that has active
-participation in the BPA Cares Award Program.
-</p>
-<p>
-An application is not required for this award. This is award is given directly from the
-National Center to the state meeting the criteria described below.
-</p>
-<h2> Ranking </h2>
-<p>
-Each state association’s ranking will be determined by:
-</p>
-<ol>
-<li> The association’s percentage of chapters participating in the following BPA Cares Award
-Programs:
-  <ul>
-     <li> Service Learning Awards Program (all awards in this category are eligible) </li>
-     <li> Marketing and Public Relations </li>
-     <li> Chapter Activities Award of Excellence </li>
-     </ul> </li>
-     <li>The total number of chapters participating in the BPA Cares Award Program within the state.</li>
-  <li>The association’s percentage of members receiving their Ambassador Torch Award.</li>
-  <li>The total number of members receiving the Ambassador Torch Award within the state.</li>
-  <li>The association’s percentage of chapters meeting the criteria for the Membership Explosion
-Award.</li>
-  <li>The total number of chapters meeting the criteria for the Membership Explosion Award within
-the state.</li>
-  <li>The association’s percentage of chapters earning the <a href="http://www.bpa.org/application/qualitychapter.aspx">Quality Chapter Distinction.</a> </li>
-  <li>The total number of chapters earning the <a href="http://www.bpa.org/application/qualitychapter.aspx">Quality Chapter Distinction.</a>Quality Chapter Distinction within the state. </li>
- </ol>
-);
-}
+        <h1> The Professional Cup Award </h1>
+        <h1> (Outstanding State Participation) </h1>
+        <h2>Purpose</h2>
+        <p>To encourage state associations to become actively involved in promoting participation in the BPA
+        Cares Award Program by the local chapters and to recognize states with active participation; and to
+        recognize outstanding state participation in BPA programs
+        </p>
+        <h2> Eligibility</h2>
+        <p>
+        Any state association in the Middle Level, Post-Secondary or Secondary Division that has active
+        participation in the BPA Cares Award Program.
+        </p>
+        <p>
+        An application is not required for this award. This is award is given directly from the
+        National Center to the state meeting the criteria described below.
+        </p>
+        <h2> Ranking </h2>
+        <p>
+        Each state association’s ranking will be determined by:
+        </p>
+        <ol>
+          <li> The association’s percentage of chapters participating in the following BPA Cares Award
+          Programs:
+            <ul>
+              <li> Service Learning Awards Program (all awards in this category are eligible) </li>
+              <li> Marketing and Public Relations </li>
+              <li> Chapter Activities Award of Excellence </li>
+            </ul>
+          </li>
+          <li>The total number of chapters participating in the BPA Cares Award Program within the state.</li>
+          <li>The association’s percentage of members receiving their Ambassador Torch Award.</li>
+          <li>The total number of members receiving the Ambassador Torch Award within the state.</li>
+          <li>The association’s percentage of chapters meeting the criteria for the Membership Explosion
+          Award.</li>
+          <li>The total number of chapters meeting the criteria for the Membership Explosion Award within
+          the state.</li>
+          <li>The association’s percentage of chapters earning the <a href="http://www.bpa.org/application/qualitychapter.aspx">Quality Chapter Distinction.</a> </li>
+          <li>The total number of chapters earning the <a href="http://www.bpa.org/application/qualitychapter.aspx">Quality Chapter Distinction.</a>Quality Chapter Distinction within the state. </li>
+        </ol>
+      </div>
+    );
+  }
 }
