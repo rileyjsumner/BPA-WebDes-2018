188f4db51c60fecf93abdde88e67eaaad7726ed4
Riley Sumner <tornadoriley@gmail.com>
2018-01-06 12:22:59 -0600

    more home csss

--- bpa-cares/src/PageComponents/HomePage.js
+++ bpa-cares/src/PageComponents/HomePage.js
@@ -2,30 +2,49 @@ import React, { Component } from 'react';
 import { Timeline } from 'react-twitter-widgets';
 import Iframe from 'react-iframe';
 import '../App.css';
+import dreambigger from '../pics/dreambigger.jpg';
 
 export class HomePage extends Component {
     render() {
         return (
-            <div className="homeContent">
-                <div id="homeRow">
-                    <div className="row">
-                        <div className="col-md-4 text-center home-div">
-                            <Timeline
-                                dataSource={{
-                                  sourceType: 'profile',
-                                  screenName: 'national_bpa'
-                                }}
-                                options={{
-                                  height: '700',
-                                  width: '300'
-                                }}
-                                onLoad={() => console.log('Timeline is loaded!')}
-                            />
+            <div className="homePageWrapper">
+                <div className="homeContent">
+                    <div id="homeRow">
+                        <div className="row home-row">
+                            <div className="col-md-4 text-center home-div">
+                                <Timeline
+                                    dataSource={{
+                                      sourceType: 'profile',
+                                      screenName: 'national_bpa'
+                                    }}
+                                    options={{
+                                      height: '700',
+                                      width: '400'
+                                    }}
+                                    onLoad={() => console.log('Timeline is loaded!')}
+                                />
+                            </div>
+                            <div className="col-md-8 text-center home-div">
+                                <h2>BPA Cares Guidelines</h2>
+                                <p>A BPA chapter aligned with the goals and mission of Business Professional of America is concerned with service. Business Professionals of America recommends chapters become involved in one or more of the BPA Cares programs it offers. Participation in this program means that members are engaging in worthwhile projects while having the opportunity to receive recognition from the national organization.<b> Be sure to share pictures of you and/or your chapter participating in various BPA Cares activities on social media, tag BPA, and use the hashtag, “#bpacares”.</b></p>
+                                <Iframe url="https://www.youtube.com/embed/ZuzE6nXl6Fg" width="90%" height="65%" allowFullScreen/>
+                            </div>
                         </div>
-                        <div className="col-md-8 text-center home-div">
-                            <h2>BPA Cares Guidelines</h2>
-                            <p>A BPA chapter aligned with the goals and mission of Business Professional of America is concerned with service. Business Professionals of America recommends chapters become involved in one or more of the BPA Cares programs it offers. Participation in this program means that members are engaging in worthwhile projects while having the opportunity to receive recognition from the national organization.<b> Be sure to share pictures of you and/or your chapter participating in various BPA Cares activities on social media, tag BPA, and use the hashtag, “#bpacares”.</b></p>
-                            <Iframe url="https://www.youtube.com/embed/ZuzE6nXl6Fg" width="90%" height="65%" allowFullScreen/>
+                    </div>
+                </div>
+                <div className="break">
+                </div>
+                <div className="homeContent">
+                    <div className="row home-row">
+                        <div className="col-md-12">
+                            <h1>Testimonials</h1>
+                            <p>This is a testimonial</p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
                         </div>
                     </div>
                 </div>
